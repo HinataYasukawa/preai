b86338c1ab9fec5bebf098411cd51c6765511ea3
HinataYasukawa <h1i2n2a62208@gmail.com>
2024-04-14 20:14:40 +0900

    get xy

--- learning.py
+++ learning.py
@@ -6,5 +6,16 @@ num = 0
 for num in range(10):
     n = f'{num:04}'
     json_open = open('frame_'+ num +'_keypoints.json')
-    json_load = json.load(json_open)
+    data = json.load(json_open)
     
+    indices = [0,4,7]
+    keypoint_names = ['face', 'right', 'left']
+    
+    for person in data['people']:
+        keypoints = person['pose_keypoints_2d']
+
+        for index, name in zip(indices, keypoint_names):
+            x = keypoints[index * 3]  # x座標
+            y = keypoints[index * 3 + 1]  # y座標
+            confidence = keypoints[index * 3 + 2]  # 信頼度
+            print(f"{name}: x = {x}, y = {y}, 信頼度 = {confidence}")
